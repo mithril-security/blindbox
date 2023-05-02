739724be22bb5ef19a6671f0abe1f9df0a978885
Matthias Brunel <mbrunel@student.42.fr>
2023-05-02 20:09:59 +0200

    simplify client

--- client/blindbox/blindbox/requests/__init__.py
+++ client/blindbox/blindbox/requests/__init__.py
@@ -1,4 +1,1 @@
-__all__ = ["SecuredSession", "post", "get", "patch", "put", "delete", "options", "head"]
-
 from .secured_session import SecuredSession
-from .requests import post, get, patch, put, delete, options, head
--- client/blindbox/tests/session.py
+++ client/blindbox/tests/session.py
@@ -1,22 +1,10 @@
-from blindbox.requests  import SecuredSession, requests
+from blindbox.requests  import SecuredSession
 
 with SecuredSession("http://localhost:8080", True) as secure_session:
-
-    res = secure_session.post(endpoint="/enclave/predict", files={"audio": open("test.wav", "rb")})
+    res = secure_session.post("/enclave/predict", files={"audio": open("test.wav", "rb")})
     print(res.status_code)
     print(res.text)
 
     res = secure_session.get("/enclave")
     print(res.status_code)
     print(res.text)
-
-
-res = requests.post(url="http://localhost:8080/enclave/predict", files={"audio": open("test.wav", "rb")})
-print(res.status_code)
-print(res.text)
-
-res = requests.get(url="http://localhost:8080/enclave")
-print(res.status_code)
-print(res.text)
-
-
