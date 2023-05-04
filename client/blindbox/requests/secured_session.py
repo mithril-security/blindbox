c66152d4e960758be63a6571691929a438b8197a
JoFrost <mebessaa@student.42.fr>
2023-05-04 13:43:40 +0000

    feat: manually apply pr#6

--- client/blindbox/requests/secured_session.py
+++ client/blindbox/requests/secured_session.py
@@ -1,10 +1,11 @@
-import requests as rq
+from requests import Session, exceptions
 import warnings
+from urllib.parse import urljoin
 
 class BlindBoxDebugModeWarning(Warning):
     pass
 
-class SecuredSession(rq.Session):
+class SecuredSession(Session):
     """A class to represent a connection to a BlindBox server."""
 
     def __init__(
@@ -21,11 +22,11 @@ class SecuredSession(rq.Session):
         """
 
         if addr == None or addr == "":
-            raise rq.exceptions.MissingSchema(
+            raise exceptions.MissingSchema(
                 "Missing URL for the Secured Session instance"
             )
 
-        self.addr = addr
+        self.base_url = addr
 
         if debug_mode:
             warnings.warn(
@@ -39,87 +40,9 @@ class SecuredSession(rq.Session):
 
         super(SecuredSession, self).__init__()
 
-    def get(self, endpoint: str = "", **kwargs):
-        r"""Sends a GET request. Returns :class:`Response` object.
-
-        :param endpoint: URL endpoint for the new :class:`Request` object.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        kwargs.setdefault("allow_redirects", True)
-        return self.request("GET", self.addr + endpoint, **kwargs)
-
-    def options(self, endpoint: str = None, **kwargs):
-        r"""Sends a OPTIONS request. Returns :class:`Response` object.
-
-        :param url: URL endpoint for the new :class:`Request` object.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        kwargs.setdefault("allow_redirects", True)
-        return self.request("OPTIONS", self.addr + endpoint, **kwargs)
-
-    def head(self, endpoint: str = "", **kwargs):
-        r"""Sends a HEAD request. Returns :class:`Response` object.
-
-        :param url endpoint: URL for the new :class:`Request` object.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        kwargs.setdefault("allow_redirects", False)
-        return self.request("HEAD", self.addr + endpoint, **kwargs)
-
-    def post(self, endpoint: str = "", data=None, json=None, **kwargs):
-        r"""Sends a POST request. Returns :class:`Response` object.
-
-        :param url endpoint: URL for the new :class:`Request` object.
-        :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-            object to send in the body of the :class:`Request`.
-        :param json: (optional) json to send in the body of the :class:`Request`.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        return self.request(
-            "POST", self.addr + endpoint, data=data, json=json, **kwargs
-        )
-
-    def put(self, endpoint: str = "", data=None, **kwargs):
-        r"""Sends a PUT request. Returns :class:`Response` object.
-
-        :param url: URL endpoint for the new :class:`Request` object.
-        :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-            object to send in the body of the :class:`Request`.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        return self.request("PUT", self.addr + endpoint, data=data, **kwargs)
-
-    def patch(self, endpoint: str = "", data=None, **kwargs):
-        r"""Sends a PATCH request. Returns :class:`Response` object.
-
-        :param url: URL endpoint for the new :class:`Request` object.
-        :param data: (optional) Dictionary, list of tuples, bytes, or file-like
-            object to send in the body of the :class:`Request`.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        return self.request("PATCH", self.addr + endpoint, data=data, **kwargs)
-
-    def delete(self, endpoint: str = "", **kwargs):
-        r"""Sends a DELETE request. Returns :class:`Response` object.
-
-        :param url endpoint: URL for the new :class:`Request` object.
-        :param \*\*kwargs: Optional arguments that ``request`` takes.
-        :rtype: requests.Response
-        """
-
-        return self.request("DELETE", self.addr + endpoint, **kwargs)
+    def request(self, method, url, *args, **kwargs):
+        joined_url = urljoin(self.base_url, url)
+        return super().request(method, joined_url, *args, **kwargs)
 
 
 def connect(addr: str, debug_mode: bool = False) -> SecuredSession:
