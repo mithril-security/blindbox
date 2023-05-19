f65a65b6caa5e16c26e9592c1f69df7feef97555
lyie28 <laurayie93@gmail.com>
2023-05-19 13:12:45 +0200

    santacoder example: fix bugs

--- examples/santacoder/server.py
+++ examples/santacoder/server.py
@@ -1,43 +1,38 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from fastapi import FastAPI
 from pydantic import BaseModel
-# import intel_extension_for_pytorch as ipex  # optional: optimize for cpu inference
 import uvicorn
 
 model_name = "bigcode/santacoder"
+
 # get tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-device = "CPU" # cuda for GPU usage or cpu for CPU usage
+
+device = "cpu" # cuda for GPU usage or cpu for CPU usage
 
 # get model and call eval
-model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True).eval()
-print("finished download running optimize")
-# optional: optimize for cpu inference
-# model = ipex.optimize(model)
-print("finished optimize")
+model = AutoModelForCausalLM.from_pretrained(model_name, trust_remote_code=True).to(device).eval()
 
+# instantiate API object
+app = FastAPI()
 
 class GenerateRequest(BaseModel):
     input_text: str
     max_new_tokens: int = 128
 
-
-app = FastAPI()
-
-
 @app.post("/generate")
 def generate(req: GenerateRequest):
 
     # We go from string to token lists
-    inputs = tokenizer(req.input_text, return_tensors="pt").to(device)
-
+    inputs = tokenizer.encode(req.input_text, return_tensors="pt").to(device)
+    
+    # query model with inputs
     outputs = model.generate(inputs)
-
+    
     # Convert tokens back to a string
     text = tokenizer.decode(outputs[0])
 
     return {"text": text}
 
-
-if name == "main":
+if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
