45bee4e95bb625afcd2209c2729f1d07664d43d1
lyie28 <laurayie93@gmail.com>
2023-05-22 12:35:11 +0200

    docs: output for qt and whisper + sample rate

---
+++ examples/whisper/query.py
@@ -0,0 +1,15 @@
+# import requests module
+from blindbox  import requests
+
+# replace the IP address here with the public IP address of your confidential VM which you can find on your Azure portal
+CONFIDENTIAL_VM_IP_ADDRESS = ""
+
+if CONFIDENTIAL_VM_IP_ADDRESS == "":
+    print("Warning!! You need to modify the query.py file to include your ip address and port for the CONFIDENTIAL_VM_IP_ADDRESS variable: e.g. \"127.0.0.1:8080\"")
+else:
+
+    # we query our application, sending it a snippet of code to be completed by the model
+    res = requests.post(url=f"http://{CONFIDENTIAL_VM_IP_ADDRESS}/whisper/predict", files={"audio": open(".tmp/test.wav", "rb")})
+
+    # display result
+    print(res.text)
