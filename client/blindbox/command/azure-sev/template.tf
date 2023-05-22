85349e88a7df55cf3d3d50507a25d487eec0ae40
shannon <shannonsaviodsouza@gmail.com>
2023-05-22 11:53:46 +0200

    updated PR

--- client/blindbox/command/azure-sev/template.tf
+++ client/blindbox/command/azure-sev/template.tf
@@ -3,6 +3,7 @@ locals {
   resource_group_name     = "blindbox-test${random_integer.group_suffix.result}"
   location                = "North Europe"
   container_registry_name = "blindbox${random_integer.group_suffix.result}"
+  attester_name           = "blindboxattester${random_integer.group_suffix.result}"
 
   # Resources
   cpu_count    = 1
@@ -59,6 +60,12 @@ provider "docker" {}
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
   location = local.location
+}
+
+resource "azurerm_attestation_provider" "blindbox_attestation" {
+  location                        = azurerm_resource_group.rg.location
+  name                            = local.attester_name
+  resource_group_name             = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_container_registry" "acr" {
@@ -247,3 +254,6 @@ output "azure_portal_url" {
   value = "${local.portal_url}/#@${local.tenant_id}/resource${local.resource_id}"
 }
 
+output "attester_url" {
+  value = azurerm_attestation_provider.blindbox_attestation.attestation_uri
+}
--- client/blindbox/tests/session.py
+++ client/blindbox/tests/session.py
@@ -1,14 +1,14 @@
 from blindbox.requests  import SecureSession, requests
 
-with SecureSession("http://20.223.63.77:80", "./cce_policy.txt",True) as secure_session:
+with SecureSession("http://localhost:8080", "./cce_policy.txt", "sampleAttester.eus.attest.azure.net", True) as secure_session:
 
-    res = secure_session.post(endpoint="/whisper/predict", files={"audio": open("test.wav", "rb")})
+    res = secure_session.post(endpoint="/enclave/predict", files={"audio": open("test.wav", "rb")})
     print(res.status_code)
     print(res.text)
 
-    #res = secure_session.get("/enclave")
-    #print(res.status_code)
-    #print(res.text)
+    res = secure_session.get("/enclave")
+    print(res.status_code)
+    print(res.text)
 
 
 res = requests.post(url="http://localhost:8080/enclave/predict", files={"audio": open("test.wav", "rb")})
