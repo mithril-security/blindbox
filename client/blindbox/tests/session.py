e5079948a97cc4eff93fc03bbaf5ae841dab1818
Andre Iglesias <ciglesia@student.42.fr>
2023-06-08 10:01:22 +0200

    ci requests debug mode

--- client/blindbox/tests/session.py
+++ client/blindbox/tests/session.py
@@ -2,8 +2,10 @@ from blindbox.requests  import SecureSession, requests
 
 SERVER_URL = "localhost"
 SERVER_PORT = 8080
+DEBUG = True
 
-with SecureSession(f"http://{SERVER_URL}:{SERVER_PORT}", "./cce_policy.txt", "sampleAttester.eus.attest.azure.net", True) as secure_session:
+
+with SecureSession(f"http://{SERVER_URL}:{SERVER_PORT}", "./cce_policy.txt", "sampleAttester.eus.attest.azure.net", DEBUG) as secure_session:
 
     res = secure_session.post(endpoint="/enclave/predict", files={"audio": open("test.wav", "rb")})
     print(res.status_code)
@@ -13,11 +15,11 @@ with SecureSession(f"http://{SERVER_URL}:{SERVER_PORT}", "./cce_policy.txt", "sa
     print(res.status_code)
     print(res.text)
 
+if DEBUG == False:
+    res = requests.post(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave/predict", cce_policy="./cce_policy.txt", files={"audio": open("test.wav", "rb")})
+    print(res.status_code)
+    print(res.text)
 
-res = requests.post(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave/predict", files={"audio": open("test.wav", "rb")})
-print(res.status_code)
-print(res.text)
-
-res = requests.get(url=f"http://{SERVER_URL}:{SERVER_PORT}/enclave")
-print(res.status_code)
-print(res.text)
+    res = requests.get(f"http://{SERVER_URL}:{SERVER_PORT}/enclave", "./cce_policy.txt")
+    print(res.status_code)
+    print(res.text)
